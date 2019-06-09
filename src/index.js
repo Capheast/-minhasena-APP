cdf6089a97a5cc2336093c6e481f1be9b7441482
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-09 01:21:38 -0300

    Create HomeScreen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './screens/HomeScreen';
 
 const MainNavigator = createStackNavigator({
-  Home: { screen: HomeScreen },
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      header: null
+    }
+  },
 });
 
 export default createAppContainer(MainNavigator);
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,41 +1,17 @@
 import React from 'react';
-import {
-  Platform, StyleSheet, Text, View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n'
-        + 'Cmd+D or shake for dev menu',
-  android:
-        'Double tap R on your keyboard to reload,\n'
-        + 'Shake or press menu button for dev menu',
-});
+import { SafeAreaView, Text } from 'react-native';
+import LotteryCard from '../components/LotteryCard';
+import Styles from '../styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <SafeAreaView style={Styles.container}>
+      <Text style={Styles.headerTitle}>Escolha o sorteio</Text>
+      <LotteryCard
+        title="Mega Sena"
+        subTitle="Sorteio atual de R$180 milhões"
+        backgroundColor="#00B758"
+      />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
