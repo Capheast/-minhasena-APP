d89b7be6b59aec93b275e371c989dad3826f11e8
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-09 23:21:14 -0300

    Add new screen in router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { FluidNavigator } from 'react-navigation-fluid-transitions';
 import HomeScreen from './screens/HomeScreen';
+import LotteryScreen from './screens/LotteryScreen';
 
-const MainNavigator = createStackNavigator({
+const MainNavigator = FluidNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
       header: null
     }
   },
+  Lottery: {
+    screen: LotteryScreen
+  }
 });
 
 export default createAppContainer(MainNavigator);
