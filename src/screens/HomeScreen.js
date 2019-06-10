8be8647c935a728cee35018a876f7d113837a4ad
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-09 23:21:30 -0300

    Mock lotteries in HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import LotteryCard from '../components/LotteryCard';
+import { SafeAreaView } from 'react-native';
 import Styles from '../styles';
+import LotteryContainer from '../components/partials/LotteryContainer';
+
+const lotteries = [{
+  title: 'Escolha o sorteio',
+  items: [{
+    title: 'Mega Sena',
+    subTitle: 'Sorteio atual de R$180 milhões',
+    color: '#00B758'
+  }, {
+    title: 'Quina',
+    subTitle: 'Sorteio atual de R$80 mil',
+    color: '#42338B'
+  }]
+}, {
+  title: 'Fique de olho',
+  items: [{
+    title: 'Lotofácil',
+    subTitle: 'Sorteio atual de R$420 mil',
+    color: '#AB1D88'
+  }]
+}, {
+  title: 'Outros',
+  items: [{
+    title: 'Dupla Sena',
+    subTitle: 'Sorteio atual de R$180 milhões',
+    color: '#A20635'
+  }]
+}];
 
 export default function App() {
   return (
     <SafeAreaView style={Styles.container}>
-      <Text style={Styles.headerTitle}>Escolha o sorteio</Text>
-      <LotteryCard
-        title="Mega Sena"
-        subTitle="Sorteio atual de R$180 milhões"
-        backgroundColor="#00B758"
-      />
+      <LotteryContainer lotteries={lotteries} />
     </SafeAreaView>
   );
 }
