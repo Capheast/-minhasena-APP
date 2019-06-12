0d23f7926aaa78913e69738c5859f3ca2a3de28e
josemiguelq <jose.miguelq95@gmail.com>
2019-06-11 22:50:49 -0300

    lenght da quantidade de numeros

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import {SafeAreaView} from 'react-native';
 import Styles from '../styles';
 import LotteryContainer from '../components/partials/LotteryContainer';
 
 const lotteries = [{
-  title: 'Escolha o sorteio',
-  items: [{
-    title: 'Mega Sena',
-    subTitle: 'Sorteio atual de R$180 milhões',
-    color: '#00B758'
-  }, {
-    title: 'Quina',
-    subTitle: 'Sorteio atual de R$80 mil',
-    color: '#42338B'
-  }]
+    title: 'Escolha o sorteio',
+    items: [{
+        title: 'Mega Sena',
+        subTitle: 'Sorteio atual de R$180 milhões',
+        color: '#00B758',
+        defaultSize: 6
+    }, {
+        title: 'Quina',
+        subTitle: 'Sorteio atual de R$80 mil',
+        color: '#42338B',
+        defaultSize: 5
+    }]
 }, {
-  title: 'Fique de olho',
-  items: [{
-    title: 'Lotofácil',
-    subTitle: 'Sorteio atual de R$420 mil',
-    color: '#AB1D88'
-  }]
+    title: 'Fique de olho',
+    items: [{
+        title: 'Lotofácil',
+        subTitle: 'Sorteio atual de R$420 mil',
+        color: '#AB1D88',
+        defaultSize: 10
+    }]
 }, {
-  title: 'Outros',
-  items: [{
-    title: 'Dupla Sena',
-    subTitle: 'Sorteio atual de R$180 milhões',
-    color: '#A20635'
-  }]
+    title: 'Outros',
+    items: [{
+        title: 'Dupla Sena',
+        subTitle: 'Sorteio atual de R$180 milhões',
+        color: '#A20635',
+        defaultSize: 10
+    }]
 }];
 
 export default function App() {
-  return (
-    <SafeAreaView style={Styles.container}>
-      <LotteryContainer lotteries={lotteries} />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={Styles.container}>
+            <LotteryContainer lotteries={lotteries}/>
+        </SafeAreaView>
+    );
 }
