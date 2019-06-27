b192117320949603f80de5169e60d25ce4d129e0
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-27 01:07:38 -0300

    Fix ButtonIcon identation

--- src/components/partials/ButtonIcon/index.js
+++ src/components/partials/ButtonIcon/index.js
@@ -1,28 +1,24 @@
-import {TouchableOpacity, View} from "react-native";
-import React from "react";
+import { TouchableOpacity, View } from 'react-native';
+import React from 'react';
 
-
-export const ButtonIcon = ({
-                               icon, onPress = () => {
-    }
-                           }) => {
-    return (
-        <TouchableOpacity
-            style={{background: '#0000001A'}}
-            onPress={onPress}
-        >
-            <View
-                style={{
-                    backgroundColor: '#24332B1A',
-                    padding: 7,
-                    borderRadius: 46,
-                    height: 50,
-                    width: 50,
-                    margin: 16,
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}>
-                {icon}
-            </View>
-        </TouchableOpacity>)
-}
+export const ButtonIcon = ({ icon, onPress = () => {} }) => (
+  <TouchableOpacity
+    style={{ background: '#0000001A' }}
+    onPress={onPress}
+  >
+    <View
+      style={{
+        backgroundColor: '#24332B1A',
+        padding: 7,
+        borderRadius: 46,
+        height: 50,
+        width: 50,
+        margin: 16,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      {icon}
+    </View>
+  </TouchableOpacity>
+);
