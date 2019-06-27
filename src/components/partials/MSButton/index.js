b431fd52131388fe34dd4e523063a1bd1bd93d4e
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-06-27 01:09:16 -0300

    Fix MSButton identation

--- src/components/partials/MSButton/index.js
+++ src/components/partials/MSButton/index.js
@@ -1,40 +1,33 @@
-import React  from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 
-export const MSButton = ({
-                             onPress = () => {
-                             },
-                             title = '',
-                             background = ''
-                         }) => {
-    return (
-        <TouchableOpacity
-            style={styles(background).container}
-            onPress={() => onPress}
-        >
-            <View style={styles(background).button}>
-                <Text style={styles(background).text}> {title} </Text>
-            </View>
-        </TouchableOpacity>
-    )
-}
+export const MSButton = ({ onPress = () => {}, title = '', background = ''}) => (
+  <TouchableOpacity
+    style={styles(background).container}
+    onPress={() => onPress}
+  >
+    <View style={styles(background).button}>
+      <Text style={styles(background).text}>{title}</Text>
+    </View>
+  </TouchableOpacity>
+);
 
-const styles = (colorPrimer) => ({
-    container: {
-        backgroundColor: colorPrimer + '1A',
-        height: 50,
-        margin: 10,
-        width:246,
-        borderRadius: 8
-    },
-    button: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        color: colorPrimer,
-        fontWeight: '500',
-        opacity: 1
-    }
-})
+const styles = colorPrimer => ({
+  container: {
+    backgroundColor: `${colorPrimer}1A`,
+    height: 50,
+    margin: 10,
+    width: 246,
+    borderRadius: 8
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: colorPrimer,
+    fontWeight: '500',
+    opacity: 1
+  }
+});
