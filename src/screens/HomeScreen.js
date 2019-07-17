9c8da6509dd47373df12a1bc6d9e5828f691b1dc
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-07-16 23:23:35 -0300

    Remove lotteries

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,5 @@
 import React, { useLayoutEffect } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
-import { useSelector } from 'react-redux';
 import { CapContainer } from '../components/ui';
 import LotteryContainer from '../components/partials/LotteryContainer';
 import Colors from '../styles/colors';
@@ -12,12 +11,10 @@ export default function HomeScreen({ navigation }) {
     });
   });
 
-  const lotteries = useSelector(({ data }) => data);
-
   return (
     <SafeAreaView style={{ backgroundColor: Colors.backgroundColor }}>
       <CapContainer mTop={56} mHorizontal={24}>
-        <LotteryContainer lotteries={lotteries} />
+        <LotteryContainer />
       </CapContainer>
     </SafeAreaView>
   );
