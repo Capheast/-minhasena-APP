9b6fe4c39ed9d7b155724cd1edeb6345ae6031f0
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-07-16 22:18:59 -0300

    Create routes folder

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { createAppContainer } from 'react-navigation';
+import { FluidNavigator } from 'react-navigation-fluid-transitions';
+import HomeScreen from '../screens/HomeScreen';
+import LotteryScreen from '../screens/LotteryScreen';
+
+const MainNavigator = FluidNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Lottery: {
+    screen: LotteryScreen
+  }
+});
+
+export default createAppContainer(MainNavigator);
