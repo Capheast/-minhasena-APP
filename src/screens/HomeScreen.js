ac7f93c7dbbb2f92dc26b7b015bd47aab7e31367
Sidney Oliveira <oliveira.sidney96@gmail.com>
2019-07-16 22:21:46 -0300

    Finish HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,54 +1,24 @@
 import React, { useLayoutEffect } from 'react';
-import { ScrollView, SafeAreaView, StatusBar } from 'react-native';
-import { withNavigation } from 'react-navigation';
-import Styles from '../styles';
+import { SafeAreaView, StatusBar } from 'react-native';
+import { useSelector } from 'react-redux';
+import { CapContainer } from '../components/ui';
 import LotteryContainer from '../components/partials/LotteryContainer';
+import Colors from '../styles/colors';
 
-const lotteries = [{
-  title: 'Escolha o sorteio',
-  items: [{
-    title: 'Mega Sena',
-    subTitle: 'Sorteio atual de R$180 milhões',
-    color: '#00B758',
-    numbersDrawn: [8, 18, 20, 24, 36, 45]
-  }, {
-    title: 'Quina',
-    subTitle: 'Sorteio atual de R$80 mil',
-    color: '#42338B',
-    numbersDrawn: [29, 33, 64, 68, 74]
-  }]
-}, {
-  title: 'Fique de olho',
-  items: [{
-    title: 'Lotofácil',
-    subTitle: 'Sorteio atual de R$420 mil',
-    color: '#AB1D88',
-    numbersDrawn: [2, 7, 8, 9, 10, 11, 16, 18, 19, 20, 21, 22, 23, 24, 25]
-  }]
-}, {
-  title: 'Outros',
-  items: [{
-    title: 'Lotomania',
-    subTitle: 'Sorteio atual de R$1.6 milhões',
-    color: '#f78100',
-    numbersDrawn: [0, 5, 14, 27, 30, 34, 36, 40, 43, 44, 55, 65, 68, 76, 78, 85, 88, 92, 98, 99]
-  }]
-}];
-
-function App({ navigation }) {
+export default function HomeScreen({ navigation }) {
   useLayoutEffect(() => {
     navigation.addListener('willFocus', () => {
       StatusBar.setBarStyle('dark-content');
     });
   });
 
+  const lotteries = useSelector(({ data }) => data);
+
   return (
-    <SafeAreaView style={{ backgroundColor: '#FFF' }}>
-      <ScrollView showsVerticalScrollIndicator={false} style={Styles.container}>
+    <SafeAreaView style={{ backgroundColor: Colors.backgroundColor }}>
+      <CapContainer mTop={56} mHorizontal={24}>
         <LotteryContainer lotteries={lotteries} />
-      </ScrollView>
+      </CapContainer>
     </SafeAreaView>
   );
 }
-
-export default withNavigation(App);
